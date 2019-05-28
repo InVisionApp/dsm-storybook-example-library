346c1ecb5a11eac828587d1f7a6e7d667072e19f
Mor Tal <morsinx@gmail.com>
2019-05-28 11:57:36 +0300

    fixed deprecate warning for a11y addon in vue SB5

--- vue/storybook-v5/.storybook/config.js
+++ vue/storybook-v5/.storybook/config.js
@@ -1,12 +1,13 @@
 import { configure, addDecorator, addParameters } from '@storybook/vue';
-import { checkA11y } from '@storybook/addon-a11y';
+import { withA11y } from '@storybook/addon-a11y';
 import { initDsm } from '@invisionapp/dsm-storybook';
 
 function loadStories() {
   require('../src/stories');
 }
 
-addDecorator(checkA11y);
+addDecorator(withA11y);
+
 //Init Dsm
 initDsm({
   addDecorator,
