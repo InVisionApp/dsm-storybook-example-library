b0687c2305fbb6e6c55b8543243b02aa075883ee
Mor Tal <morsinx@gmail.com>
2019-09-10 19:42:32 +0300

    changed button to CSF format and added addon-docs with simple doc

--- react/storybook-v5/src/components/Button.stories.js
+++ react/storybook-v5/src/components/Button.stories.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, boolean, select, text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import buttonDocs from './Button.mdx';
 
-import Button from '../components/Button';
+import Button from './Button';
 
-storiesOf('Button', module)
-  .addDecorator(withKnobs)
-  .add(
-    'Button',
-    () => {
-      const iconOptions = { none: null, 'chevron-right': 'chevron-right' };
+export default {
+  title: 'Button',
+  parameters: {
+    component: Button,
+    decorators: [withKnobs],
+    // Module-Level 'in-dsm' configuration (Will apply to all stories inside the module)
+    'in-dsm': { id: '5cf53658fbb8682a3246d907', versionFilePath: '../components/versionFile.json' }
+  }
+};
 
-      return (
-        <Button
-          onClick={() => action('Button clicked')('Click')}
-          icon={select('icon', iconOptions, iconOptions.none)}
-          disabled={boolean('disabled', false)}
-        >
-          {text('children', 'TEXT')}
-        </Button>
-      );
-    },
-    {
-      'in-dsm': { id: '5cf53658fbb8682a3246d907', versionFilePath: '../components/versionFile.json' }
-    }
-  );
+const iconOptions = { none: null, 'chevron-right': 'chevron-right' };
+
+export const simpleButton = () => (
+  <Button
+    onClick={() => action('Button clicked')('Click')}
+    icon={select('icon', iconOptions, iconOptions.none)}
+    disabled={boolean('disabled', false)}
+  >
+    {text('children', 'TEXT')}
+  </Button>
+);
+
+simpleButton.story = {
+  parameters: {
+    docs: { page: buttonDocs }
+  }
+};
