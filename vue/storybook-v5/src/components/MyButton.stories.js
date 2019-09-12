263a6bf82540b6772f6feb769775a4640478cbfc
Mor Tal <morsinx@gmail.com>
2019-09-12 14:32:42 +0300

    converted button story in vue to CSF

--- vue/storybook-v5/src/components/MyButton.stories.js
+++ vue/storybook-v5/src/components/MyButton.stories.js
@@ -1,30 +1,31 @@
-import { storiesOf } from '@storybook/vue';
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import MyButton from '../components/MyButton.vue';
 
-storiesOf('Button', module)
-  .addDecorator(withKnobs)
-  .add(
-    'Button',
-    () => {
-      const iconValues = ['none', 'chevron-right'];
-      return {
-        components: { MyButton },
-        props: {
-          textKnob: { default: text('text', 'TEXT') },
-          disabledKnob: { default: boolean('disabled', false) },
-          iconKnob: { default: select('icon', iconValues, 'none') }
-        },
-        methods: { buttonClick: () => action('Button clicked')('Click') },
-        template: '<my-button :onClick=buttonClick :icon=iconKnob :disabled=disabledKnob>{{textKnob}}</my-button>'
-      };
-    },
-    {
-      'in-dsm': {
-        id: '5cf53658fbb8682a3246d907',
-        componentPath: '../components/MyButton.vue',
-        versionFilePath: '../components/versionFile.json'
-      }
+export default {
+  title: 'Button',
+  decorators: [withKnobs],
+  parameters: {
+    // Module-Level 'in-dsm' configuration (Will apply to all stories inside the module)
+    'in-dsm': {
+      id: '5c86295038392c00aafece83',
+      componentPath: '../components/MyButton.vue',
+      versionFilePath: '../components/versionFile.json'
     }
-  );
+  }
+};
+
+const iconValues = ['none', 'chevron-right'];
+
+export const simpleButton = () => {
+  return {
+    components: { MyButton },
+    props: {
+      textKnob: { default: text('text', 'TEXT') },
+      disabledKnob: { default: boolean('disabled', false) },
+      iconKnob: { default: select('icon', iconValues, 'none') }
+    },
+    methods: { buttonClick: () => action('Button clicked')('Click') },
+    template: '<my-button :onClick=buttonClick :icon=iconKnob :disabled=disabledKnob>{{textKnob}}</my-button>'
+  };
+};
