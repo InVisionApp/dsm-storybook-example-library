4a93eb8164a837a147940fa5ea5ae1bf1f0dcd7c
Mor Tal <morsinx@gmail.com>
2019-09-12 11:17:08 +0300

    changed html framework button to CSF as well

--- html/storybook-v5/src/stories/button.stories.js
+++ html/storybook-v5/src/stories/button.stories.js
@@ -1,44 +1,51 @@
-import { storiesOf } from '@storybook/html';
 import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
 import { decorate } from '@storybook/addon-actions';
 import { chevronRightIcon } from '../components/utils/icons';
 import '../components/button/_button.scss';
+import buttonDocs from '../components/button/button.mdx';
+
+export default {
+  title: 'Button',
+  parameters: {
+    decorators: [withKnobs],
+    // Module-Level 'in-dsm' configuration (Will apply to all stories inside the module)
+    'in-dsm': { id: '5c86295038392c00aafece83' }
+  }
+};
 
 const decoratedAction = decorate([() => ['Click']]);
 
-storiesOf('Button', module)
-  .addDecorator(withKnobs)
-  .add(
-    'Button',
-    () => {
-      const buttonIcons = {
-        'chevron-right': chevronRightIcon
-      };
+const buttonIcons = {
+  'chevron-right': chevronRightIcon
+};
 
-      const availableIcons = {
-        none: '',
-        'chevron-right': 'chevron-right'
-      };
+const availableIcons = {
+  none: '',
+  'chevron-right': 'chevron-right'
+};
 
-      const icon = select('icon', availableIcons, availableIcons.none);
-      const disabled = boolean('disabled', false);
-      const children = text('text', 'TEXT');
+export const simpleButton = () => {
+  const icon = select('icon', availableIcons, availableIcons.none);
+  const disabled = boolean('disabled', false);
+  const children = text('text', 'TEXT');
 
-      return decoratedAction.withActions({ 'click .dsm-btn': 'Button clicked!' })(
-        () => `
+  return decoratedAction.withActions({ 'click .dsm-btn': 'Button clicked!' })(
+    () => `
 <div class="dsm-container">
     <button class="dsm-btn${icon !== availableIcons.none ? ' dsm-btn--with-icon' : ''}" ${disabled ? 'disabled' : ''}>
       <div class="dsm-btn__content">${children}</div>
       ${icon !== availableIcons.none ? buttonIcons[icon] : ''}
     </button>
 </div>`
-      );
-    },
-    {
-      'in-dsm': {
-        id: '5ce17ca24d0426001d9d3599',
-        docFilePath: '../components/button/button.docs.json',
-        containerClass: 'dsm-container'
-      }
-    }
   );
+};
+
+simpleButton.story = {
+  parameters: {
+    'in-dsm': {
+      docFilePath: '../components/button/button.docs.json',
+      containerClass: 'dsm-container'
+    },
+    docs: { page: buttonDocs }
+  }
+};
