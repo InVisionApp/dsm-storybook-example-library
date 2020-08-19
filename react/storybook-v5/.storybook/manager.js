54485d6bb8ebb7952d98b47494bc7208f7e4f201
byronguina <byron.guina@gmail.com>
2020-08-19 11:15:33 -0500

    Update comments in example library for overwriting setConfig

--- react/storybook-v5/.storybook/manager.js
+++ react/storybook-v5/.storybook/manager.js
@@ -1,6 +1,23 @@
 import { addons } from '@storybook/addons';
 import { getDefaultDsmOptions, getDefaultDsmTheme } from '@invisionapp/dsm-storybook';
 
+/**
+ * To override @invisionapp/dsm-storybook custom options/theme you can use Storybook options parameter and theming
+ * -- this will only override the specific parameters you change --
+ * options: https://storybook.js.org/docs/react/configure/features-and-behavior
+ * theming: https://storybook.js.org/docs/react/configure/theming
+ * Example code below
+ **/
+//  addons.setConfig({
+//    ...getDefaultDsmOptions(process.env.STORYBOOK_DSM),
+//    showNav: true,
+//    theme: {
+//      ...getDefaultDsmTheme(process.env.STORYBOOK_DSM),
+//      fontBase: "'Open Sans'",
+//      colorPrimary: "#123456"
+//    }
+//  });
+
 addons.setConfig({
   ...getDefaultDsmOptions(process.env.STORYBOOK_DSM),
   theme: {
--- react/storybook-v5/.storybook/preview.js
+++ react/storybook-v5/.storybook/preview.js
@@ -3,21 +3,6 @@ import { withA11y } from '@storybook/addon-a11y';
 import centered from '@storybook/addon-centered/react';
 import { withDsm } from '@invisionapp/dsm-storybook/withDsm';
 
-/**
- * To override @invisionapp/dsm-storybook custom options\theme you can use Storybook options parameter and theming
- * -- this will only override the specific parameters you change --
- * options: https://github.com/storybooks/storybook/blob/master/docs/src/pages/configurations/options-parameter/index.md
- * theming: https://github.com/storybooks/storybook/blob/master/docs/src/pages/configurations/theming/index.md
- * Example code below
- **/
-// import { themes } from '@storybook/theming';
-//
-// function setCustomOptions() {
-//   addParameters({
-//     options: { theme: { ...themes.dark, brandTitle: 'Custom Title!' } }
-//   });
-// }
-
 addParameters({ docs: { page: null } });
 addDecorator(withA11y);
 addDecorator(centered);
