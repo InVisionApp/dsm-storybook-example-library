18dc947a5ae4bdf02a8b26eb70b14f4b66dbd567
byronguina <byron.guina@gmail.com>
2020-09-17 09:23:24 -0500

    Unify theming example to new application suggestion

--- angular/storybook-v6/.storybook/manager.js
+++ angular/storybook-v6/.storybook/manager.js
@@ -19,9 +19,7 @@ import { getDsmOptions, getDsmTheme } from '@invisionapp/dsm-storybook';
 //  });
 
 addons.setConfig({
-  ...getDsmOptions(process.env.STORYBOOK_DSM),
+  ...getDsmOptions(process.env.STORYBOOK_DSM)
   // Using the DSM theme is optional
-  theme: {
-    ...getDsmTheme(process.env.STORYBOOK_DSM)
-  }
+  // ...getDsmTheme(process.env.STORYBOOK_DSM)
 });
--- html/storybook-v6/.storybook/manager.js
+++ html/storybook-v6/.storybook/manager.js
@@ -21,5 +21,5 @@ import { getDsmOptions, getDsmTheme } from '@invisionapp/dsm-storybook';
 addons.setConfig({
   ...getDsmOptions(process.env.STORYBOOK_DSM)
   // Using the DSM theme is optional
-  // theme: getDsmTheme(process.env.STORYBOOK_DSM)
+  // ...getDsmTheme(process.env.STORYBOOK_DSM)
 });
