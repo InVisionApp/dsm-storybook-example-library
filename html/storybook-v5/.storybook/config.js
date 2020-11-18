b0186211e32dd479844c478080b5e01820b7c914
byronguina <byron.guina@gmail.com>
2020-11-18 15:19:54 -0600

    Add missing withDsm decorator in SB5 example

--- html/storybook-v5/.storybook/config.js
+++ html/storybook-v5/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure, addDecorator, addParameters } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import centered from '@storybook/addon-centered/html';
-import { initDsm } from '@invisionapp/dsm-storybook';
+import { initDsm, withDsm } from '@invisionapp/dsm-storybook';
 
 /**
  * To override @invisionapp/dsm-storybook custom options\theme you can use Storybook options parameter and theming
@@ -19,12 +19,16 @@ import { initDsm } from '@invisionapp/dsm-storybook';
 // }
 
 addParameters({
-  backgrounds: [{ name: 'DSM background', value: '#f8f8fa', default: true }, { name: 'dark', value: '#333' }]
+  backgrounds: [
+    { name: 'DSM background', value: '#f8f8fa', default: true },
+    { name: 'dark', value: '#333' }
+  ]
 });
 
 addParameters({ docs: { page: null } });
 addDecorator(withA11y);
 addDecorator(centered);
+addDecorator(withDsm);
 
 //Init Dsm
 initDsm({
