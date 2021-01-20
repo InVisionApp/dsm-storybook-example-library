42d519ae3fa4de605d87d8d3a1205e4814e8805d
byronguina <byron.guina@gmail.com>
2021-01-20 14:08:20 -0600

    Add individual story parameters comment

--- react/storybook-v6/src/components/Toast.stories.js
+++ react/storybook-v6/src/components/Toast.stories.js
@@ -26,3 +26,11 @@ ToastComponent.args = {
   text: 'This is a notification message',
   status: statusOptions.default
 };
+
+ToastComponent.story = {
+  parameters: {
+    // Story-Level 'in-dsm' configuration (Will apply only to the story that is being configured)
+    // Story-Level configuration will override Module-Level 'in-dsm' configuration for the specific story
+    // 'in-dsm': { id: '5d81da1f794992009993e356', versionFilePath: './versionFile.json' },
+  }
+};
